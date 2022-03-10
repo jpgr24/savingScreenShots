0f6ddd268a4829fd9f049caf7873586657869805
jpgr24 <jgroening@yahoo.com>
2022-03-09 22:17:06 -0800

    final

--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -1,15 +1,18 @@
 const LoginPage = require('../pageobjects/login.page');
 const SecurePage = require('../pageobjects/secure.page');
+const DynamicLoadingPage = require('../pageobjects/dynamic_loading.page');
 
-describe('My Login application', () => {
-    xit('should login with valid credentials', async () => {
-        await LoginPage.open();
 
-        await LoginPage.login('tomsmith', 'SuperSecretPassword!');
-        await expect(SecurePage.flashAlert).toBeExisting();
-        await expect(SecurePage.flashAlert).toHaveTextContaining(
-            'You logged into a secure area!');
+describe('My Dynamic Loading Page', () => {
+    DynamicLoadingPage.exist('dynamic_loading/1','#start button','start button exists')
+     it('click', async () => {
+       
+        await DynamicLoadingPage.Click('dynamic_loading/1','#start button')
+        
+        await expect(DynamicLoadingPage.htest).toBeDisplayed();
+        await browser.saveScreenshot('./screenshot/screenshot2.png');
     });
 });
+
 
 
